b8c6662578edacaffefa95ff75ba5cada4554b78
Montana Mellett <mell0050@algonquinlive.com>
2019-02-14 12:36:02 -0500

    adding router implementation

---
+++ src/components/pages/dashboard-page/dashboard-page.component.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'DashboardPage',
+}
+
+// PascalConvention = for Class as in object oriented
+// kebab-convent = for template or template class
+// camelCaseConvent = for JS vars and ids in template
---
+++ src/components/pages/list-page/list-page.component.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="listPage">
+    listpage boom
+  </div>
+</template>
+<script src="./list-page.component.js"></script>
+<style src="./list-page.component.css"></style>
